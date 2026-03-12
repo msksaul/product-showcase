450b71e0088d4a4d284e715df9b65cdf0ef5b73d
Melvin Saúl <msksaul@gmail.com>
2026-03-12 15:00:15 -0600

    hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Outfit, Geist } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -18,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en">
       <body
         className={`${outfit.className} antialiased`}
       >
