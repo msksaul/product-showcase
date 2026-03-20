ce6fef2d1d8a6694162b08d5f51abcc99fe86ed3
Melvin Saúl <msksaul@gmail.com>
2026-03-20 16:40:48 -0600

    skeleton for dynamic content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedProducts from '@/components/landing-page/featured-products';
 import HeroSection from '@/components/landing-page/hero-section';
 import RecentlyLaunchedProducts from '@/components/landing-page/recently-launched-products';
+import ProductSkeleton from '@/components/products/product-skeleton';
 import { Suspense } from 'react';
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
     <div>
       <HeroSection />
       <FeaturedProducts />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<ProductSkeleton />}>
         <RecentlyLaunchedProducts />
       </Suspense>
     </div>
