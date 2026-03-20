064ac41efdf05b4b758245785755b4180f78ea68
Melvin Saúl <msksaul@gmail.com>
2026-03-20 16:04:15 -0600

    zod validations

--- lib/products/product-actions.ts
+++ lib/products/product-actions.ts
@@ -1,26 +1,84 @@
 'use server'
 
 import { FormState } from '@/types'
-import { auth } from '@clerk/nextjs/server'
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { productSchema } from './product-validations'
+import { db } from '@/db'
+import { products } from '@/db/schema'
+import z from 'zod'
 
-export const addProductAction = async (prevState: FormState, formData: FormData) => {
+export const addProductAction = async (_prevState: FormState, formData: FormData) => {
   try {
-    const { userId } = await auth()
+    const { userId, orgId } = await auth()
 
-    if(!userId) {
+    if (!userId) {
       return {
         success: false,
-        message:'You must be signed in to submit a product'
-      }
+        message: "You must be signed in to submit a product",
+        errors: undefined,
+      };
     }
 
+    if (!orgId) {
+      return {
+        success: false,
+        message: "You must be a member of an organization to submit a product",
+        errors: undefined,
+      };
+    }
+
+    const user = await currentUser();
+    const userEmail = user?.primaryEmailAddress?.emailAddress || "anonymous";
+
     const rawFormData = Object.fromEntries(formData.entries())
+
+    const validatedData = productSchema.safeParse(rawFormData)
+
+    if(!validatedData.success) {
+      console.log(validatedData.error.flatten().fieldErrors);
+      return {
+        success: false,
+        errors: validatedData.error.flatten().fieldErrors,
+        message: "Invalid data",
+      };
+    }
+
+    const { name, slug, tagline, description, websiteUrl, tags } = validatedData.data
+
+    const tagsArray = tags ? tags.filter((tag => typeof tag === 'string')) : []
+
+    await db.insert(products).values({
+      name,
+      slug,
+      tagline,
+      description,
+      websiteUrl,
+      tags: tagsArray,
+      status: 'pending',
+      submittedBy: userEmail,
+      organizationId: orgId,
+      userId
+    })
+
+    return {
+      success: true,
+      message: 'Product submitted successfully!, It will be reviewed shortly.',
+      errors: undefined
+    }
   } catch (error) {
     console.error(error)
 
+    if (error instanceof z.ZodError) {
+      return {
+        success: false,
+        errors: error.flatten().fieldErrors,
+        message: "Validation failed. Please check the form.",
+      };
+    }
+
     return {
       success: false,
-      errors: error,
+      errors: undefined,
       message: 'Failed to submit product'
     }
   }
